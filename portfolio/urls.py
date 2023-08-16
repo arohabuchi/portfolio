9ed44a3e546d99c4d287a667277cabea76d6e77b
arohabuchi <arohabuchi@gmail.com>
2023-08-16 02:52:52 -0700

    ready for python anywhere deployments

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Project.urls'))
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 admin.site.site_header = "Abuchi Blog"
